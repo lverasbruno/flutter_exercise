d293e9e54c767730ec6820ba5a7d8f0153086228
Bruno Lingerfelt Veras <lverasbruno@gmail.com>
2025-12-03 16:41:06 -0300

    feat: widgetbook config

--- widgetbook/lib/exercise_1.dart
+++ widgetbook/lib/exercise_1.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:formfun_flutter_test/exercise_1.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'Default', type: Exercise1)
 Widget buildExercise1(BuildContext context) {
-  return const Exercise1();
+  final percentage = context.knobs.double.slider(
+    label: 'Progress',
+    initialValue: 0,
+    min: 0,
+    max: 100,
+  );
+
+  return Exercise1(percentage: percentage);
 }
-
-class Exercise1 extends StatelessWidget {
-  const Exercise1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Exercise 1',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
